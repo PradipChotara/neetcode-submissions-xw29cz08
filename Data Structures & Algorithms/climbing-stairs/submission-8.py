f1e756bd0c9695b0d28e419e221831cc298763fa
PradipChotara <61057766+PradipChotara@users.noreply.github.com>
2026-05-31 16:06:55 +0530

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.py
@@ -0,0 +1,27 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        dp = [0] * (n+1)
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
+
+        # def solve(i):
+
+        #     if i > n:
+        #         return 0
+            
+        #     if i == n:
+        #         return 1
+
+        #     left = solve(i+1)
+        #     right = solve(i+2)
+
+        #     return left + right
+
+        # return solve(0)
